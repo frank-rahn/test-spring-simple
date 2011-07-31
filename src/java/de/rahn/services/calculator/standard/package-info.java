5364ff8cdbb695de6bac649e04bbe3f065308d07
Frank W. Rahn <frank@frank-rahn.de>
2011-07-31 16:49:48 +0000

    Merge aus dem Branch b1.0

--- src/java/de/rahn/services/calculator/standard/package-info.java
+++ src/java/de/rahn/services/calculator/standard/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Eine einfache Implementierung des Taschenrechnerservices.
+ * Eine Implementierung des Taschenrechnerservices.
  * @author Frank W. Rahn
  * @see de.rahn.services.calculator.Calculator
  */
